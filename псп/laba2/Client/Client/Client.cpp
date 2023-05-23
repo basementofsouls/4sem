a6fdbec29129526c37fdd80539d476b8f49de7ea
Stacy <nat.kkurag.0301@mail.ru>
2023-05-24 02:37:35 +0300

    pupupu

---
+++ псп/laba2/Client/Client/Client.cpp
@@ -0,0 +1,77 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <iostream>
+#include <clocale>
+#include <ctime>
+
+#include "ErrorMsgText.h"
+#include "Winsock2.h"                
+#pragma comment(lib, "WS2_32.lib")   
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    WSADATA wsaData;
+    SOCKET cC;
+    SOCKADDR_IN serv;
+    serv.sin_family = AF_INET;
+    serv.sin_port = htons(2000);
+    serv.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    try {
+        std::cout << "ClientT\n\n";
+
+        if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0) {
+            throw  set_error_msg_text("Startup: ", WSAGetLastError());
+        }
+        if ((cC = socket(AF_INET, SOCK_STREAM, NULL)) == INVALID_SOCKET) {
+            throw  set_error_msg_text("socket: ", WSAGetLastError());
+        }
+        if ((connect(cC, (sockaddr*)&serv, sizeof(serv))) == SOCKET_ERROR) {
+            throw set_error_msg_text("connect: ", WSAGetLastError());
+        }
+
+        char ibuf[50] = "server: принято ";
+        int  libuf = 0, lobuf = 0;
+
+        int message_amount;
+        std::cout << "Amount of messages to send: ";
+        std::cin >> message_amount;
+
+        const clock_t start = clock();
+        for (int i = 1; i <= message_amount; i++) {
+            std::string obuf = "Hello from Client " + std::to_string(i);
+
+            if ((lobuf = send(cC, obuf.c_str(), strlen(obuf.c_str()) + 1, NULL)) == SOCKET_ERROR) {
+                throw set_error_msg_text("send: ", WSAGetLastError());
+            }
+
+            if ((libuf = recv(cC, ibuf, sizeof(ibuf), NULL)) == SOCKET_ERROR) {
+                throw set_error_msg_text("recv: ", WSAGetLastError());
+            }
+
+            std::cout << ibuf << '\n';
+        }
+        const clock_t end = clock();
+        const std::string obuf = "";
+
+        if ((lobuf = send(cC, obuf.c_str(), strlen(obuf.c_str()) + 1, NULL)) == SOCKET_ERROR) {
+            throw set_error_msg_text("send: ", WSAGetLastError());
+        }
+
+        std::cout << "Full time of recv/send exchange: " << (static_cast<double>(end - start) / CLK_TCK) << " c\n";
+
+        if (closesocket(cC) == SOCKET_ERROR) {
+            throw set_error_msg_text("closesocket: ", WSAGetLastError());
+        }
+        if (WSACleanup() == SOCKET_ERROR) {
+            throw  set_error_msg_text("Cleanup: ", WSAGetLastError());
+        }
+    }
+    catch (std::string error_msg_text) {
+        WSACleanup();
+        std::cout << '\n' << error_msg_text;
+    }
+    return 0;
+}
